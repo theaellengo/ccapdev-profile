1fe1be5c41de8088abc15c4fa2edf74927bf829e
Thea Go <theaellengo@gmail.com>
2021-02-20 16:49:42 +0800

    added register, login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,44 @@
 <template>
+  <Navbar />
   <div class="login">
-    login
+    <div class="container">
+      <div class="form-container">
+        <!--Alert-->
+        <div class="alert alert-text">
+          Invalid Credetials.
+        </div>
+        <h1 class="large text-dark">
+          Sign In
+        </h1>
+        <p class="lead">
+          <i class="fas fa-user"></i>
+          Sign into your account
+        </p>
+        <form action="home.html" class="form">
+          <div class="form-group">
+            <label class="control-label" for="email">Email:</label>
+            <input type="email" placeholder="Email Address" />
+          </div>
+          <div class="form-group">
+            <label class="control-label" for="pwd">Password:</label>
+            <input type="password" placeholder="Password" min-length="6" />
+          </div>
+          <input type="submit" value="Sign In" class="btn btn-primary" />
+        </form>
+        <p class="my-1">
+          Don't have an account? <a href="register.html">Register here.</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Navbar from '@/components/Navbar.vue';
   export default {
-    name: 'Login'
+    name: 'Login',
+    components: {
+      Navbar
+    }
   };
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,11 +1,62 @@
 <template>
+  <Navbar />
   <div class="register">
-    register
+    <div class="container">
+      <div class="form-container">
+        <!--Alert-->
+        <div class="alert alert-text">
+          Invalid Credetials.
+        </div>
+        <h1 class="large text-dark">
+          Sign Up
+        </h1>
+        <p class="lead">
+          <i class="fas fa-user"></i>
+          Create your account
+        </p>
+        <form action="index.html" class="form">
+          <div class="form-group">
+            <label class="control-label" for="username">Username:</label>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div class="form-group">
+            <label class="control-label" for="idno">ID no:</label>
+            <input type="number" placeholder="ID number" />
+          </div>
+          <div class="form-group">
+            <label class="control-label" for="email">Email:</label>
+            <input type="email" placeholder="Email Address" />
+          </div>
+          <div class="form-group">
+            <label class="control-label" for="password">Password:</label>
+            <input type="password" placeholder="Password" min-length="6" />
+          </div>
+          <div class="form-group">
+            <label class="control-label" for="password"
+              >Confirm Password:</label
+            >
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              min-length="6"
+            />
+          </div>
+          <input type="submit" value="Register" class="btn btn-primary" />
+        </form>
+        <p class="my-1">
+          Already have an account? <a href="login.html">Login here.</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Navbar from '@/components/Navbar.vue';
   export default {
-    name: 'Register'
+    name: 'Register',
+    components: {
+      Navbar
+    }
   };
 </script>
