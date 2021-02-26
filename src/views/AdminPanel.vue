0828a2687c9ae616698ebe381f2113759e6c4a3b
Thea Go <theaellengo@gmail.com>
2021-02-26 18:06:23 +0800

    add prof to admin panel

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,11 +1,51 @@
 <template>
   <div class="admin-panel">
     search
+    <div class="addbtn">
+      <button class="btn" id="show-modal" @click="showModal = !showModal">
+        Add Professor
+      </button>
+    </div>
+    <div v-if="showModal" class="dark-overlay">
+      <div class="addmodal">
+        <ProfEdit
+          :viewdata="showAddModal"
+          @clickedItem="showAddModal = !showAddModal"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import ProfAdd from '@/components/ProfAdd.vue';
   export default {
-    name: 'adminPanel'
+    name: 'adminPanel',
+    data: () => {
+      return {
+        idNum: 0,
+        showAddModal: false
+      };
+    },
+    components: {
+      ProfAdd
+    }
   };
 </script>
+
+<style scoped>
+  .addbtn {
+    float: right;
+  }
+  .addmodal {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 60%;
+  }
+  .dark-overlay {
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+</style>
