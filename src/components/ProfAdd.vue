cce567a877330384d7ad822eadcbb0bbaeae6427
Thea Go <theaellengo@gmail.com>
2021-02-26 18:01:09 +0800

    add prof

--- src/components/ProfAdd.vue
+++ src/components/ProfAdd.vue
@@ -1,31 +1,57 @@
 <template>
-  <div class="card">
-    <h1 class="large text-dark">Add Professor</h1>
-    <form action="index.html" class="form">
-      <div class="form-group">
-        <label class="control-label" for="username">Name:</label>
-        <input type="text" placeholder="Name" />
+  <modal v-if="viewdata">
+    <div class="card">
+      <div class="header">
+        <span><h1 class="large text-dark">Add Professor</h1></span>
+        <span>
+          <button class="btn exit" @click="$emit('clickedItem', viewdata)">
+            <i class="fas fa-times"></i>
+          </button>
+        </span>
       </div>
-      <div class="form-group">
-        <label class="control-label" for="idno">College:</label>
-        <input type="text" placeholder="College" />
-      </div>
-      <div class="form-group">
-        <label class="control-label" for="email">Department:</label>
-        <input type="text" placeholder="Department" />
-      </div>
-      <div class="form-group">
-        <label class="control-label" for="email">Courses:</label>
-        <input type="text" placeholder="Courses" />
-      </div>
-      <br />
-      <input type="submit" value="Add Professor" class="btn btn-primary" />
-    </form>
-  </div>
+      <form action="index.html" class="form">
+        <div class="form-group">
+          <label class="control-label" for="username">Name:</label>
+          <input type="text" placeholder="Name" />
+        </div>
+        <div class="form-group">
+          <label class="control-label" for="idno">College:</label>
+          <input type="text" placeholder="College" />
+        </div>
+        <div class="form-group">
+          <label class="control-label" for="email">Department:</label>
+          <input type="text" placeholder="Department" />
+        </div>
+        <div class="form-group">
+          <label class="control-label" for="email">Courses:</label>
+          <input type="text" placeholder="Courses" />
+        </div>
+        <br />
+        <input type="submit" value="Delete Professor" class="btn" />
+        <input type="submit" value="Done Editing" class="btn btn-primary" />
+      </form>
+    </div>
+  </modal>
 </template>
 
 <script>
   export default {
-    name: 'ProfAdd'
+    name: 'ProfEdit',
+    props: ['viewdata']
   };
 </script>
+
+<style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+  .exit {
+    background: transparent;
+    border: none;
+  }
+  .exit:hover {
+    color: #255075;
+    background-color: #f7f7f7;
+  }
+</style>
